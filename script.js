027aa0219e71b135b5b2f12d684b99b28616713b
Farruh <farruh020202@gmail.com>
2022-08-29 10:36:06 +0500

    anketa done

---
+++ script.js
@@ -0,0 +1,21 @@
+const firstName = prompt("Введите свое имя");
+const lastName = prompt("Введите свою фамилию");
+const age = prompt("Введите свой возраст");
+const study = prompt("Где вы учитесь?");
+const favColour = prompt("Какой ваш любимый цвет?");
+const favNumber = prompt("Какое ваше любимое число?");
+
+console.log(
+  "Ваше имя - " +
+    firstName +
+    ", ваша фамилия - " +
+    lastName +
+    ", ваш возраст - " +
+    age +
+    ", вы учитесь в - " +
+    study +
+    ", ваш любимый цвет - " +
+    favColour +
+    ", ваше любимое число - " +
+    favNumber
+);
